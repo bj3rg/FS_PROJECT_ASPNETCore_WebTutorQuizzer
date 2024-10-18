c2917586b2b0d5b6f6305d4d0e7d671d22661539
Angelo Burog <angelokburog30@gmail.com>
2024-10-18 13:28:48 +0800

    initialize MVC structure

---
+++ FS_PROJECT_ASPNETCore_WebTutorQuizzer/Controllers/HomeController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace FS_PROJECT_ASPNETCore_WebTutorQuizzer.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
