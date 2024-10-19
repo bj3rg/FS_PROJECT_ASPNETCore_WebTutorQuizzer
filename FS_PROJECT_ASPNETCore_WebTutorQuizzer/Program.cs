ef336eddd5fd425bc9058c0d42aef903eb872932
Angelo Burog <angelokburog30@gmail.com>
2024-10-19 20:02:52 +0800

    feat: configure middleware and services

--- FS_PROJECT_ASPNETCore_WebTutorQuizzer/Program.cs
+++ FS_PROJECT_ASPNETCore_WebTutorQuizzer/Program.cs
@@ -1,9 +1,15 @@
+using FS_PROJECT_ASPNETCore_WebTutorQuizzer.Data;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddControllersWithViews();
+builder.Services.AddDbContext<DataContext>(options => options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"))); 
 
 var app = builder.Build();
-
+app.UseStaticFiles();
+app.UseRouting();
 app.MapDefaultControllerRoute();
+
 
 app.Run();
