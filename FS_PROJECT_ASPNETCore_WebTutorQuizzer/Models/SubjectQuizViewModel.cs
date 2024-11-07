ead0836109f3dc0aaf7542b44f54f819ad94c0f1
Angelo Burog <angelokburog30@gmail.com>
2024-11-07 13:27:27 +0800

    feat(model): add SubjectQuizViewModel for quiz and subject filtering

---
+++ FS_PROJECT_ASPNETCore_WebTutorQuizzer/Models/SubjectQuizViewModel.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc.Rendering;
+
+namespace FS_PROJECT_ASPNETCore_WebTutorQuizzer.Models
+{
+    public class SubjectQuizViewModel
+    {
+        public List<QuizModel> Quizs  {  get; set; }
+        public SelectList? Subjects { get;set; }
+        public string? QuizSubject { get; set; }
+        public string? SearchString { get; set; }
+    }
+}
